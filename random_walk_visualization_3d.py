1ddf33587c1ed1950aef2da6a9644e92ac6f1b16
zhiwen <zhiwen@smartx.com>
2019-08-16 18:44:11 +0800

    make sync

--- random_walk_visualization_3d.py
+++ random_walk_visualization_3d.py
@@ -1,23 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-x = np.random.randn(1000, 2)
+
+x = np.random.randn(1000, 3)
 w = np.cumsum(x, axis=0)
 
 plt.ion()
-
+fig= plt.figure(figsize=(8, 8))
+# plt.axis("off")
 for i in range(len(w)-1):
     x1 = w[i]
     x2 = w[i+1]
-    plt.scatter(*x2, color="blue")
-    plt.plot([x1[0], x2[0]], [x1[1], x2[1]], color="blue")
+    x = [x1[0], x2[0]]
+    y = [x1[1], x2[1]]
+    z = [x1[2], x2[2]]
+
+    ax = fig.gca(projection='3d')
+    ax.scatter(x2[0], x2[1], x2[2], color="blue")
+    ax.plot(x, y, z, color='blue')
+    ax.grid(False)
     plt.pause(0.01)
 
 plt.ioff()
 plt.show()
-
-def plot_3d_features(self, model):
-    plt.clf()
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.gca(projection='3d')
-    ax.plot(x, y, z, color='blue')
